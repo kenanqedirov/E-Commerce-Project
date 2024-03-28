513e399d0d0db7c7ae559f5d27e08df140c9b33f
kenanqedirov <k.qedirov1999@gmail.com>
2024-03-28 16:55:03 +0400

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductDetail
+    {
+        public string ProductDetailId { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+
+    }
+}
