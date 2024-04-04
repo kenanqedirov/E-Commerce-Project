4acf6667719613e4c0c971f27d43c0692259480a
kenanqedirov <k.qedirov1999@gmail.com>
2024-04-04 19:13:09 +0400

    Order Service Added .Using CQRS ,Onion Architecture

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/UpdateOrderDetailCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers
+{
+    public class UpdateOrderDetailCommandHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public UpdateOrderDetailCommandHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(UpdateOrderDetailCommand updateOrderDetailCommand)
+        {
+            var value = await _repository.GetByIdAsync(updateOrderDetailCommand.OrderDetailId);
+
+            value.OrderingId = updateOrderDetailCommand.OrderingId;
+            value.ProductId = updateOrderDetailCommand.ProductId;
+            value.ProductName = updateOrderDetailCommand.ProductName;
+            value.ProductPrice = updateOrderDetailCommand.ProductPrice;
+            value.ProductTotalPrice = updateOrderDetailCommand.ProductTotalPrice;
+            value.ProductAmount = updateOrderDetailCommand.ProductAmount;
+
+            await _repository.UpdateAsync(value);
+        }
+    }
+}
---
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Controllers/AddressesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+
+namespace MultiShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly GetAddressQueryHandler _getAddressQueryHandler;
+        private readonly GetAddressByIdQueryHandler _getAddressByIdQueryHandler;
+        private readonly CreateAddressCommandHandler _createAddressCommandHandler;
+        private readonly UpdateAddressCommandHandler _updateAddressCommandHandler;
+        private readonly RemoveAddressCommandHandler _removeAddressCommandHandler;
+        public AddressesController(GetAddressQueryHandler getAddressQueryHandler, GetAddressByIdQueryHandler getAddressByIdQueryHandler, RemoveAddressCommandHandler removeAddressCommandHandler, UpdateAddressCommandHandler updateAddressCommandHandler, CreateAddressCommandHandler createAddressCommandHandler)
+        {
+            _getAddressQueryHandler = getAddressQueryHandler;
+            _getAddressByIdQueryHandler = getAddressByIdQueryHandler;
+            _removeAddressCommandHandler = removeAddressCommandHandler;
+            _updateAddressCommandHandler = updateAddressCommandHandler;
+            _createAddressCommandHandler = createAddressCommandHandler;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddressList()
+        {
+            var values = await _getAddressQueryHandler.Handle();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAddressById(int id)
+        {
+            var values = await _getAddressByIdQueryHandler.Handle(new GetAddressByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAddress(CreateAddressCommand createAddressCommand)
+        {
+            await _createAddressCommandHandler.Handle(createAddressCommand);
+            return Ok("Successfully Added");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateAddress(UpdateAddressCommand updateAddressCommand)
+        {
+            await _updateAddressCommandHandler.Handle(updateAddressCommand);
+            return Ok("Successfully updated");
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveAddress(int id)
+        {
+            await _removeAddressCommandHandler.Handle(new RemoveAddressCommand(id));
+            return Ok("Successfully Removed");  
+        }
+    }
+}
