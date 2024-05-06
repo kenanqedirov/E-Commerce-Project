ea0244326520730ca21acfd30b55a6bce3e02db5
kenanqedirov <k.qedirov1999@gmail.com>
2024-05-06 16:57:12 +0400

    Products , Product Details divided ViewComponents

---
+++ Frontend/MultiShop.WebUI/ViewComponents/ProductListComponents/_ProductListSizeFilterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListComponents
+{
+    public class _ProductListSizeFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
